6eef3422d3cdc46c836d8755444e309f92f8bc8b
Thomas Wang <thomas@teaminkling.com>
2021-08-24 17:00:03 +1000

    Get an index page looking more or less correct
    This includes with working nav on both mobile and desktop with active selection

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+/**
+ * A footer globally present for the entire website.
+ */
+export default class Footer extends Component {
+  /**
+   * @returns {JSX.Element} the {@link JSX.Element}
+   */
+  render(): JSX.Element {
+    const currentYear: number = new Date().getFullYear();
+
+    return (
+      <footer className={"content"}>
+        <p>Paced Engineering &copy; 2015 - {currentYear}.</p>
+      </footer>
+    );
+  }
+}
