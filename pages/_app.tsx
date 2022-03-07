1083d329d23aabf97edae568d4bc467c60d88e2f
Thomas Wang <thomas@teaminkling.com>
2022-03-08 04:15:18 +1100

    Delete the navbar and simplify to a one-pager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,10 @@
-import { motion } from "framer-motion";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import Footer from "../components/footer";
-import Navbar from "../components/navbar";
 import "../styles/globals.scss";
 
 // eslint-disable-next-line require-jsdoc
-function App({ Component, pageProps, router }: AppProps): JSX.Element {
-  const variants = {
-    hidden: { y: 20 },
-    enter: { y: 0 },
-  };
-
+function App({ Component, pageProps }: AppProps): JSX.Element {
   // noinspection HtmlRequiredTitleElement
   return (
     <div id={"base"}>
@@ -36,17 +29,10 @@ function App({ Component, pageProps, router }: AppProps): JSX.Element {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="theme-color" content="#FF00E6" />
       </Head>
-      <Navbar />
-      <motion.main
-        key={router.route}
-        variants={variants}
-        initial={"hidden"}
-        animate={"enter"}
-        transition={{ duration: 0.2 }}
-      >
+      <main>
         <Component {...pageProps} />
         <Footer />
-      </motion.main>
+      </main>
     </div>
   );
 }
