d7accd26a51837e7bd576aae95b3fbed561054ab
paced <paced@users.noreply.github.com>
2024-02-20 01:16:10 +0000

    deploy: e86495867090e78b8a5ac56bab3c70436b8f9f43

--- _next/static/zqcrknExhd3BCWE09XcHN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2cca2479-0bbb46a5c792a454.js","static/chunks/603-458824c38e4336f8.js","static/chunks/pages/index-bc90d6d06ae60163.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
