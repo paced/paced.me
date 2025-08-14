926ccc6e197bbedf70198db09686bdbca5956957
Thomas Wang <thomas@paced.me>
2025-08-14 20:09:00 +1000

    Upgrade several major dependencies as per semver (#149)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,6 +1,8 @@
 import vikeReact from "vike-react/config";
+import type { Config } from "vike/types";
 
 export const config = {
   extends: vikeReact,
+  prerender: true,
   ssr: false,
-};
+} satisfies Config;
