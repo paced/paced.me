697db0a619695243575c729cce97e4bafcc1b2eb
Thomas Wang <github@paced.me>
2026-03-07 18:34:58 +1100

    Refactor footer component to include GitHub source

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="content">
-      <p>Paced Engineering &copy; 2015 - {new Date().getFullYear()}.</p>
+      <p>
+        Paced Engineering &copy; 2015 - {new Date().getFullYear()},
+        <a
+          href="https://github.com/paced/paced.me"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ marginLeft: "0.5ch" }}
+        >
+          <FaGithub size={9} /> Source
+        </a>
+      </p>
     </footer>
   );
 }
